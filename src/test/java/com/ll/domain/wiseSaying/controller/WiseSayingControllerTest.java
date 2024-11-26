6ddbd7c04d7cc90cf82b453afd6258ebfe8e1d61
Spinichi <spinichi@naver.com>
2024-11-27 00:29:08 +0900

    work

--- src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -1,5 +1,6 @@
 package com.ll.domain.wiseSaying.controller;
 
+import com.ll.App.App;
 import com.ll.standard.util.TestUtil;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -25,5 +26,25 @@ public class WiseSayingControllerTest {
 
         assertThat(output)
                 .contains("== 명언 앱 ==");
+    }
+
+    @Test
+    @DisplayName("명령) ")
+    public void t2() {
+        Scanner scanner = TestUtil.getScanner("""
+                목록
+                종료
+                """); // 입력을 대신 넣어줌
+        ByteArrayOutputStream outputstream = TestUtil.setOutToByteArray(); // 출력을 대신 받아줌
+
+        App app = new App(scanner);
+        app.run();
+
+        String output = outputstream.toString();
+
+        TestUtil.clearSetOutToByteArray(outputstream); // 출력 비워줌(원상복구)
+
+        assertThat(output)
+                .contains("명령) ");
     }
 }
