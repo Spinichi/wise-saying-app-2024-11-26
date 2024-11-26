f19ee1851b7fc2826c0988f214fc083b1c002c76
Spinichi <spinichi@naver.com>
2024-11-26 17:32:29 +0900

    work

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,8 +1,19 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        lab1();
 
     }
 
+    private static void lab1() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("명령) ");
+        String cmd = scanner.nextLine().trim();
+
+        System.out.println("입력한 명령: " + cmd);
+    }
 }
