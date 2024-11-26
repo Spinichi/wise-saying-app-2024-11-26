7b062c66be3144569a0574c1c51e26aec8f5254a
Spinichi <spinichi@naver.com>
2024-11-26 17:38:09 +0900

    work

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,19 +1,41 @@
 package com.ll;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        lab1();
+        //lab1();
+        lab2();
 
     }
 
+    // 사람의 입력을 scan
     private static void lab1() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("명령) ");
+        System.out.print("명령) ");
         String cmd = scanner.nextLine().trim();
 
         System.out.println("입력한 명령: " + cmd);
+    }
+
+    //scanner는 in을 스캔함
+    private static void lab2() {
+        InputStream in = new ByteArrayInputStream("안녕\n잘가".getBytes());
+
+        Scanner scanner = new Scanner(in);
+
+        System.out.println("명령) ");
+        String cmd = scanner.nextLine().trim(); // in을 받아옴
+
+        System.out.println("입력한 명령: " + cmd);
+
+        System.out.println("명령) ");
+        cmd = scanner.nextLine().trim(); // in을 받아옴
+
+        System.out.println("입력한 명령: " + cmd);
+
     }
 }
