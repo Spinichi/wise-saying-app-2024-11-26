d14dba4bfde1b010eb79439e270520e4e81f3235
Spinichi <spinichi@naver.com>
2024-11-27 00:11:08 +0900

    work

--- src/test/java/com/ll/standard/util/TestUtil.java
+++ src/test/java/com/ll/standard/util/TestUtil.java
@@ -1,9 +1,25 @@
 package com.ll.standard.util;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class TestUtil {
     public static Scanner getScanner(String input) {
         return new Scanner(input);
+    }
+
+    public static ByteArrayOutputStream setOutToByteArray() {
+        ByteArrayOutputStream output = new ByteArrayOutputStream(); // output 안에 쌓임
+        System.setOut(new PrintStream(output)); // 기존의 out이 교체됨
+        return output;
+    }
+
+    public static void clearSetOutToByteArray(ByteArrayOutputStream byteArrayOutputStream) {
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        try {
+             byteArrayOutputStream.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
